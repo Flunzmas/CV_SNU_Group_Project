964aa2ac4485cd9f8751958f587265230bde29ae
Andreas Boltres <andreas.boltres@posteo.de>
2018-06-05 17:32:43 +0900

    added scale retrieval of resistors to facilitate sliding-window template matching.

--- src/matlab/+im_analysis/detect_inductors.m
+++ src/matlab/+im_analysis/detect_inductors.m
@@ -1,4 +1,11 @@
 % TODO inductor = a series of connected half-circles
-function ec_inductors = detect_inductors(lines, corners, circles)
+function ec_inductors = detect_inductors(axis_lines, corners, circles)
+
+% check for at least three equal distances between circle centers
+circ_distances = squareform(pdist(circles(:, 1:2)));
+distance_buddies = im_analysis.find_distance_buddies(circ_distances, 3);
+
+
+
 
 ec_inductors = 0;
