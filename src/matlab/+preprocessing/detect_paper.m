76901bd7a1656ec82b3375c94897fa32bffaa8c5
Andreas Boltres <andreas.boltres@posteo.de>
2018-06-07 20:46:22 +0900

    integrated image selection and ocr into the main program flow.

--- src/matlab/+preprocessing/detect_paper.m
+++
@@ -1,8 +0,0 @@
-% Extracts the corners of the paper displayed in given image, assuming the
-% surroundings of the paper are darker.
-function p_correspondences = detect_paper(im_original)
-
-p_correspondences = [0 0 0 0;
-                     0 0 0 0];
-
-% TODO
