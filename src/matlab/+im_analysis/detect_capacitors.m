74a49f4d1e7f77395810421305161b80b2766d79
Andreas Boltres <andreas.boltres@posteo.de>
2018-06-08 09:09:45 +0900

    small cleanup

--- src/matlab/+im_analysis/detect_capacitors.m
+++
@@ -1,4 +0,0 @@
-% TODO capacitor = 2 closely parallel lines, corner points form a rectangle
-function ec_capacitors = detect_capacitors(lines, corners, circles)
-
-ec_capacitors = 0;
--- src/matlab/+im_analysis/detect_sources.m
+++
@@ -1,4 +0,0 @@
-% TODO sources = big circle with a '+' on one side of the inside and a '-' on the other.
-function ec_sources = detect_sources(lines, corners, circles)
-
-ec_sources = 0;
